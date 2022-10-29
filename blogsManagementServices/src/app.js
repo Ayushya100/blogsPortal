a7da8110ecb5d62fbc58e5427d0954d8dc3cb1a8
Ayushya <jaiswalayushya@gmail.com>
2022-10-29 23:53:31 +0530

    Initialized blogsManagementServices

---
+++ blogsManagementServices/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Adding Middlewares - Cors
+app.use(cors());
+app.use(bodyparser.json());
+
+// Adding required connection
+require('../connections/dbConnection');
+
+// Port No and API
+const port = process.env.port || 3000;
+const blogsApi = '/api/v1/blogs';
+const tagsApi = '/api/v1/tags';
+const collectionsApi = '/api/v1/collections';
+
+// Routes
+
+
+app.listen(port, () => {
+    console.log(`Connection has been started at port: ${port}`);
+});
+
+module.exports = app;
