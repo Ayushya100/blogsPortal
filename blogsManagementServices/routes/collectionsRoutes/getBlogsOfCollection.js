698714f7d0ed6ad888a2dd94e4f881d88ebbcafe
Ayushya <jaiswalayushya@gmail.com>
2022-10-30 20:44:06 +0530

    Added getBlogsOfCollectionService

--- blogsManagementServices/routes/collectionsRoutes/getBlogsOfCollection.js
+++ blogsManagementServices/routes/collectionsRoutes/getBlogsOfCollection.js
@@ -9,7 +9,14 @@ router.get('/:id', async(req, res) => {
         const _id = req.params.id;
         const collections = await Collections.findById(_id);
         if (collections != null) {
-            
+            const collectionList = collections.collectionList;
+            let result = [];
+
+            for (let index = 0; index < collectionList.length; index++) {
+                const notes = await Notes.findById(collectionList[index]);
+                result.push(notes);
+            }
+            res.status(200).send(result);
         } else {
             res.status(404).send('Requested Collection not found');
         }
