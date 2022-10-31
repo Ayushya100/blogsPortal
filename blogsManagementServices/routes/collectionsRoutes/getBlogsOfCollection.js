791a9649d50d8fdc576f389dbaba101a5a444d37
Ayushya <jaiswalayushya@gmail.com>
2022-10-31 23:40:24 +0530

    Added getBlogsOfCollectionService

--- blogsManagementServices/routes/collectionsRoutes/getBlogsOfCollection.js
+++ blogsManagementServices/routes/collectionsRoutes/getBlogsOfCollection.js
@@ -3,13 +3,20 @@ const router = express.Router();
 
 // Adding Models
 const Collections = require('../../models/collectionsModels');
+const Blogs = require('../../models/blogsModels');
 
 router.get('/:id', async(req, res) => {
     try {
         const _id = req.params.id;
         const collections = await Collections.findById(_id);
         if (collections != null) {
-            
+            const collectionList = collections.collectionList;
+            let result = [];
+            for (let index = 0; index < collectionList.length; index++) {
+                const blog = await Blogs.findById(collectionList[index]);
+                result.push(blog);
+            }
+            res.status(200).send(result);
         } else {
             res.status(404).send('Requested Collection not found');
         }
