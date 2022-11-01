fc9434aa08f5b854688660359789bb4b7a0695a8
Ayushya <jaiswalayushya@gmail.com>
2022-11-01 19:29:25 +0530

    Initialized coreModule

--- learnAndGrowPortal/src/app/modules/core/core-routing.module.ts
+++ learnAndGrowPortal/src/app/modules/core/core-routing.module.ts
@@ -1,5 +1,15 @@
 import { Routes } from "@angular/router";
 
+// Component
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+
 export const coreRoutes: Routes = [
-    
+    {
+        path: '', component: PageNotFoundComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: '**', redirectTo: '/',
+        pathMatch: 'full'
+    }
 ];
--- learnAndGrowPortal/src/app/modules/core/core.module.ts
+++ learnAndGrowPortal/src/app/modules/core/core.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+// Components
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BlogsModule } from '../blogs/blogs.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    MainComponent,
+    PageNotFoundComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    SharedModule,
+    BlogsModule
+  ],
+  providers: [],
+  exports: [
+    MainComponent
   ]
 })
 export class CoreModule { }
